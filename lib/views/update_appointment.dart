9a698fac299253df69cb50d3c797160e5f38d97b
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-12-20 09:52:27 -0600

    UI_BF1_Appointments

--- lib/views/update_appointment.dart
+++ lib/views/update_appointment.dart
@@ -52,110 +52,104 @@ class _UpdateAppointmentState extends State<UpdateAppointment> {
     _titleCtrl.text = 'Flutter Exam';
     _descrCtrl.text = 'Flutter class in ITK. Exam this Monday! Wake upr early!';
 
-    return HideKeyboard(
-      child: SafeArea(
-        child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: AppBar(
-              backgroundColor: ColorManager.lightGreen,
-              title: Text(AppString.updateAppoint,
-                  style: Theme.of(context).textTheme.headline2),
-            ),
-            body: Column(
-              children: <Widget>[
-                Container(
-                  color: ColorManager.lightGreen,
-                  height: AppSize.s110,
-                  width: double.infinity,
+    return SafeArea(
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            backgroundColor: ColorManager.lightGreen,
+            title: Text(AppString.updateAppoint,
+                style: Theme.of(context).textTheme.headline2),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                color: ColorManager.lightGreen,
+                height: AppSize.s110,
+                width: double.infinity,
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(AppPadding.p14,
+                    AppPadding.p28, AppPadding.p24, AppPadding.p14),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      AppString.title,
+                      style: CustomTextStyle.appointmentTitles,
+                    ),
+                    TextField(
+                      controller: _titleCtrl,
+                      keyboardType: TextInputType.text,
+                      style: (TextStyle(color: ColorManager.darkGreen)),
+                      decoration: InputDecoration(
+                        hintText: AppString.title,
+                        hintStyle: TextStyle(color: ColorManager.darkPink),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: AppSize.s24,
+                    ),
+                    Text(AppString.appointment,
+                        style: CustomTextStyle.appointmentTitles),
+                    TextField(
+                      controller: _descrCtrl,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 5,
+                      style: (TextStyle(color: ColorManager.darkGreen)),
+                      decoration: InputDecoration(
+                        hintText: AppString.descr,
+                        hintStyle: TextStyle(color: ColorManager.darkPink),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: AppSize.s40,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        OutlinedButton(
+                            onPressed: _showDatePicker,
+                            child: const Text(AppString.date)),
+                        const SizedBox(
+                          width: AppSize.s28,
+                        ),
+                        Text(
+                          dateNow,
+                          style: CustomTextStyle.appointmentDate,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: AppSize.s28,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        OutlinedButton(
+                            onPressed: _showTimePicker,
+                            child: const Text(AppString.time)),
+                        const SizedBox(
+                          width: AppSize.s28,
+                        ),
+                        Text(timeNow,
+                            style: Theme.of(context).textTheme.bodyText2)
+                      ],
+                    ),
+                    const SizedBox(height: AppSize.s52),
+                    ElevatedButton(
+                        onPressed: () {
+                          DialogManager().sucessDialog(
+                              context,
+                              AppString.upSuccess,
+                              AppRoutes.homeScreen,
+                              AppString.home);
+                          print(dateNow);
+                          print(timeNow);
+                        },
+                        child: const Text(AppString.updateAppoint)),
+                  ],
                 ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(AppPadding.p14,
-                      AppPadding.p28, AppPadding.p24, AppPadding.p14),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        AppString.title,
-                        style: CustomTextStyle.appointmentTitles,
-                      ),
-                      TextField(
-                        controller: _titleCtrl,
-                        keyboardType: TextInputType.text,
-                        style: (TextStyle(color: ColorManager.darkGreen)),
-                        decoration: InputDecoration(
-                          hintText: AppString.title,
-                          hintStyle: TextStyle(color: ColorManager.darkPink),
-                          //labelText: AppString.title,
-                          //helperText: AppString.requiredAdd
-                        ),
-                      ),
-                      const SizedBox(
-                        height: AppSize.s24,
-                      ),
-                      Text(AppString.appointment,
-                          style: CustomTextStyle.appointmentTitles),
-                      TextField(
-                        controller: _descrCtrl,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 5,
-                        style: (TextStyle(color: ColorManager.darkGreen)),
-                        decoration: InputDecoration(
-                          hintText: AppString.descr,
-                          hintStyle: TextStyle(color: ColorManager.darkPink),
-                          //labelText: AppString.descr,
-                          //helperText: AppString.requiredAdd
-                        ),
-                      ),
-                      const SizedBox(
-                        height: AppSize.s40,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          OutlinedButton(
-                              onPressed: _showDatePicker,
-                              child: const Text(AppString.date)),
-                          const SizedBox(
-                            width: AppSize.s28,
-                          ),
-                          Text(
-                            dateNow,
-                            style: CustomTextStyle.appointmentDate,
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: AppSize.s28,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          OutlinedButton(
-                              onPressed: _showTimePicker,
-                              child: const Text(AppString.time)),
-                          const SizedBox(
-                            width: AppSize.s28,
-                          ),
-                          Text(timeNow,
-                              style: Theme.of(context).textTheme.bodyText2)
-                        ],
-                      ),
-                      const SizedBox(height: AppSize.s52),
-                      ElevatedButton(
-                          onPressed: () {
-                            DialogManager().sucessDialog(
-                                context,
-                                AppString.upSuccess,
-                                AppRoutes.homeScreen,
-                                AppString.home);
-                            print(dateNow);
-                            print(timeNow);
-                          },
-                          child: const Text(AppString.updateAppoint)),
-                    ],
-                  ),
-                )
-              ],
-            )),
-      ),
+              )
+            ],
+          )),
     );
   }
 }

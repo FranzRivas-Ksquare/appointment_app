e39dfb47a302d7e02a1a753ad3ea72d32e8439c7
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-12-20 19:25:43 -0600

    dateTime_format

--- lib/resources/router.dart
+++ lib/resources/router.dart
@@ -22,7 +22,7 @@ var routes = <String, WidgetBuilder>{
   SignUp.routeName: (ctx) => const SignUp(),
   Home.routeName: (ctx) => const Home(),
   Profile.routeName: (ctx) => const Profile(),
-  UpdateProfile.routeName: (ctx) => UpdateProfile(),
+  UpdateProfile.routeName: (ctx) => const UpdateProfile(),
   NewAppointment.routeName: (ctx) => const NewAppointment(),
   UpdateAppointment.routeName: (ctx) => const UpdateAppointment(),
 };

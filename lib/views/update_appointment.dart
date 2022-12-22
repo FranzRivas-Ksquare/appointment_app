e9b053f85d6927bfb1cca432e0a0aa0c734540e6
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-12-22 11:30:49 -0600

    bugFix_texfield_UA

--- lib/views/update_appointment.dart
+++ lib/views/update_appointment.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '../custom_widgets/dialog_manager.dart';
+import '../custom_widgets/hideKeyboard_custom.dart';
+import '../custom_widgets/textfield_custom.dart';
 import '../resources/color_manager.dart';
 import '../resources/routes_manager.dart';
 import '../resources/string_manager.dart';
@@ -83,114 +85,105 @@ class _UpdateAppointmentState extends State<UpdateAppointment> {
       });
     }
 
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: ColorManager.lightGreen,
-          title: Text(AppString.updateAppoint,
-              style: Theme.of(context).textTheme.headline2),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              color: ColorManager.lightGreen,
-              height: AppSize.s110,
-              width: double.infinity,
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(AppPadding.p14, AppPadding.p28,
-                  AppPadding.p24, AppPadding.p14),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    AppString.title,
-                    style: CustomTextStyle.appointmentTitles,
-                  ),
-                  TextField(
-                    controller: _titleCtrl,
-                    keyboardType: TextInputType.text,
-                    style: (TextStyle(color: ColorManager.darkGreen)),
-                    decoration: InputDecoration(
-                      hintText: AppString.title,
-                      hintStyle: TextStyle(color: ColorManager.darkPink),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: AppSize.s24,
-                  ),
-                  Text(AppString.appointment,
-                      style: CustomTextStyle.appointmentTitles),
-                  TextField(
-                    controller: _descrCtrl,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 5,
-                    style: (TextStyle(color: ColorManager.darkGreen)),
-                    decoration: InputDecoration(
-                      hintText: AppString.descr,
-                      hintStyle: TextStyle(color: ColorManager.darkPink),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: AppSize.s40,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      OutlinedButton(
-                          onPressed: _showDatePicker,
-                          child: const Text(AppString.date)),
-                      const SizedBox(
-                        width: AppSize.s28,
-                      ),
-                      Text(
-                        dateNow,
-                        style: CustomTextStyle.appointmentDate,
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: AppSize.s28,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      OutlinedButton(
-                          onPressed: _showTimePicker,
-                          child: const Text(AppString.time)),
-                      const SizedBox(
-                        width: AppSize.s28,
-                      ),
-                      Text(timeNow,
-                          style: Theme.of(context).textTheme.bodyText2)
-                    ],
-                  ),
-                  const SizedBox(height: AppSize.s52),
-                  ElevatedButton(
-                      // TODO: received the appointment ID to update the appointment
-                      onPressed: () async {
-                        Appointment updateAppointment = Appointment(
-                          id: args[
-                              'id'], // TODO: To update the appointment, need the ID here
-                          title: _titleCtrl.text,
-                          description: _descrCtrl.text,
-                          date: '$dateNow $timeNow',
-                          author: dataServices.getCurrentUser.email,
-                        );
-                        if (await dataServices
-                            .updateAppointments(updateAppointment)) {
-                          DialogManager().sucessDialog(
-                              context,
-                              AppString.upSuccess,
-                              AppRoutes.homeScreen,
-                              AppString.home);
-                        }
-                        print(dateNow);
-                        print(timeNow);
-                      },
-                      child: const Text(AppString.updateAppoint)),
-                ],
+    return HideKeyboard(
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            backgroundColor: ColorManager.lightGreen,
+            title: Text(AppString.updateAppoint,
+                style: Theme.of(context).textTheme.headline2),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                color: ColorManager.lightGreen,
+                height: AppSize.s110,
+                width: double.infinity,
               ),
-            )
-          ],
-        ));
+              Container(
+                padding: const EdgeInsets.fromLTRB(AppPadding.p14,
+                    AppPadding.p28, AppPadding.p24, AppPadding.p14),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      AppString.title,
+                      style: CustomTextStyle.appointmentTitles,
+                    ),
+                    CustomTitleField(
+                      controller: _titleCtrl,
+                      hintText: AppString.title,
+                    ),
+                    const SizedBox(
+                      height: AppSize.s24,
+                    ),
+                    Text(AppString.appointment,
+                        style: CustomTextStyle.appointmentTitles),
+                    CustomAppointmentField(
+                      controller: _descrCtrl,
+                      hintText: AppString.appointment,
+                    ),
+                    const SizedBox(
+                      height: AppSize.s40,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        OutlinedButton(
+                            onPressed: _showDatePicker,
+                            child: const Text(AppString.date)),
+                        const SizedBox(
+                          width: AppSize.s28,
+                        ),
+                        Text(
+                          dateNow,
+                          style: CustomTextStyle.appointmentDate,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: AppSize.s28,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        OutlinedButton(
+                            onPressed: _showTimePicker,
+                            child: const Text(AppString.time)),
+                        const SizedBox(
+                          width: AppSize.s28,
+                        ),
+                        Text(timeNow,
+                            style: Theme.of(context).textTheme.bodyText2)
+                      ],
+                    ),
+                    const SizedBox(height: AppSize.s52),
+                    ElevatedButton(
+                        // TODO: received the appointment ID to update the appointment
+                        onPressed: () async {
+                          Appointment updateAppointment = Appointment(
+                            id: args[
+                                'id'], // TODO: To update the appointment, need the ID here
+                            title: _titleCtrl.text,
+                            description: _descrCtrl.text,
+                            date: '$dateNow $timeNow',
+                            author: dataServices.getCurrentUser.email,
+                          );
+                          if (await dataServices
+                              .updateAppointments(updateAppointment)) {
+                            DialogManager().sucessDialog(
+                                context,
+                                AppString.upSuccess,
+                                AppRoutes.homeScreen,
+                                AppString.home);
+                          }
+                          print(dateNow);
+                          print(timeNow);
+                        },
+                        child: const Text(AppString.updateAppoint)),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
 }

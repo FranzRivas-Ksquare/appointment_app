c46df4c453f978821cff2f26f0b39eb80e121231
Franz Sinisco <sinisco@tutanota.com>
2022-12-29 11:13:32 -0600

    minor changes

--- lib/controller/timeratio_provider.dart
+++ lib/controller/timeratio_provider.dart
@@ -5,28 +5,31 @@ import '../custom_widgets/time_ratio.dart';
 class TimeRatioProvider extends ChangeNotifier {
 
   static final List timesText = ["All", "Today", "Tomorrow", "Past"];
+  int index = 0;
 
-  List<TimeRatio> timeRatioButtons = [];
+  List<TimeRatio> _timeRatioButtons = [];
+
+  List<TimeRatio> get timeRatioButtons => _timeRatioButtons;
 
   void fillTimeRatioArray() {
-    if (timeRatioButtons.isEmpty) {
-      timeRatioButtons =
+    if (_timeRatioButtons.isEmpty) {
+      _timeRatioButtons =
           timesText
               .map((e) => TimeRatio(text: e, isSelect: false))
               .toList();
     }
-    timeRatioButtons.elementAt(0).isSelect = true;
+    _timeRatioButtons.elementAt(0).isSelect = true;
     notifyListeners();
   }
 
   void changeTimeRatio(int index) {
-    timeRatioButtons[index].isSelect = true;
-    for (var element in timeRatioButtons) {
-      if (element.isSelect && element != timeRatioButtons[index]) {
+    for (var element in _timeRatioButtons) {
         element.isSelect = false;
-      }
     }
-    if (kDebugMode) print(timeRatioButtons[index].text);
+    this.index = index;
+    _timeRatioButtons[index].isSelect = true;
+    if (kDebugMode) print(_timeRatioButtons[index].text);
     notifyListeners();
   }
+
 }
